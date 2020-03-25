be798edde748b55bd6cae788f233851946ecf86f
dmeadows <meadows.david@siemens.com>
2020-03-25 15:03:37 -0400

    Section 7: 47. Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -4,10 +4,29 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'));
-
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'David Meadows',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'David Meadows'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpText: 'This is a help message.'
+    })
+});
 
 app.get('/weather', (req, res) => {
     res.send({
