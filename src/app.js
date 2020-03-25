0f237302ba696649f3758e735f779af57846e12f
dmeadows <meadows.david@siemens.com>
2020-03-25 12:11:10 -0400

    Section 7: 44. Serving up HTML and JSON

--- src/app.js
+++ src/app.js
@@ -3,19 +3,30 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Hello express!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help page');
+    res.send([
+        {
+            name: 'David',
+            age: 27,
+        },
+        {
+            name: 'Paula',
+            age: 32
+        }]);
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page!');
+    res.send('<h1>About Weather App</h1>');
 });
 
 app.get('/weather', (req, res) => {
-    res.send('This is the weather page! YAY!');
+    res.send({
+        location: "Jacksonville",
+        forecast: "It's HOT!",
+    });
 });
 
 app.listen(3000, () => {
